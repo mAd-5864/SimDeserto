ab0a83c6c6fd3864f956c97b5fa7ffc9fdcce4dc
Pedro Amorim <pepas52@gmail.com>
2024-12-03 18:36:32 +0000

    funcoes para cidade

--- include/Cidade.h
+++ include/Cidade.h
@@ -5,8 +5,22 @@
 #ifndef SIMDESERTO_CIDADE_H
 #define SIMDESERTO_CIDADE_H
 
+#include "Utils.h"
 
 class Cidade {
+private:
+    char nome;  // Nome da cidade
+    std::map<std::string, bool> caravanasDisponiveis;  // Tipo de caravana e disponibilidade
+
+public:
+    // Construtor
+    explicit Cidade(char nome);
+
+    // Mostra caravanas disponíveis
+    void mostrarCaravanasDisponiveis() const;
+
+    // Compra uma caravana
+    bool comprarCaravana(char tipo);
 
 };
 
--- src/Cidade.cpp
+++ src/Cidade.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "../include/Cidade.h"
+
+
+// Construtor: inicializa as caravanas disponíveis
+Cidade::Cidade(char nome) : nome(nome) {
+    caravanasDisponiveis = {
+            {"Mercadoria", true},
+            {"Militar",    true},
+            {"Secreta",    true}
+    };
+}
+
+// Mostra as caravanas disponíveis na cidade
+void Cidade::mostrarCaravanasDisponiveis() const {
+    std::cout << "Caravanas disponíveis na cidade " << nome << ":\n";
+    for (const auto &caravana: caravanasDisponiveis) {
+        std::cout << "  " << caravana.first << " - "
+                  << (caravana.second ? "Disponível" : "Indisponível") << '\n';
+    }
+}
+
+// Compra uma caravana, se disponível
+bool Cidade::comprarCaravana(char tipo) {
+    std::string nomeCaravana = converterTipoCaravana(tipo);
+    if (nomeCaravana.empty()) {
+        std::cerr << "[ERRO] Tipo de caravana inválido. <C>-Comercio <M>-Militar <S>-Secreta \n";
+        return false;
+    }
+
+    auto it = caravanasDisponiveis.find(nomeCaravana);
+    if (it != caravanasDisponiveis.end() && it->second) {
+        it->second = false; // Marca a caravana como comprada
+        std::cout << "Caravana do tipo " << nomeCaravana << " comprada na cidade " << nome << std::endl;
+        return true;
+    }
+
+    std::cerr << "[ERRO] Caravana do tipo " << nomeCaravana
+              << " não está disponível na cidade " << nome << std::endl;
+    return false;
+}
