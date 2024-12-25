ac58f0b02f05bb33929d19e9775e773a984e01a9
Pedro Amorim <pepas52@gmail.com>
2024-12-25 11:21:05 +0000

    automove caravana comercio

--- src/Caravanas/CaravanaComercio.cpp
+++ src/Caravanas/CaravanaComercio.cpp
@@ -19,8 +19,8 @@ void CaravanaComercio::atualizarTurno() {
         // mover aleatoriamente
         if (instantesRestantes-- == 0);// passados 5 instantes, acaba por desaparecer
     } else {
-            if (numTripulantes < tripulantesMax / 2) qntAgua--;
-            else qntAgua -= 2;
+        if (numTripulantes < tripulantesMax / 2) qntAgua--;
+        else qntAgua -= 2;
         if (qntAgua < 0) {
             numTripulantes--;
             qntAgua = 0;
@@ -29,4 +29,44 @@ void CaravanaComercio::atualizarTurno() {
     movesRestantes = maxMoves;
 }
 
-std::pair<int, int> CaravanaComercio::autoMove(std::vector<std::unique_ptr<Caravana>> &caravanas, std::vector<Barbaro> &barbaros){}
+std::pair<int, int>CaravanaComercio::autoMove(std::vector<std::unique_ptr<Caravana>> &caravanas, std::vector<Barbaro> &barbaros) {
+    // Procurar caravanas a 6 posições de distancia
+    int alidado = 0, linhaAliado, colunaAliado;
+    int distanciaMin = 999, distancia;
+    for (const auto &caravana: caravanas) {
+        if (caravana->getId() != ID) {
+
+            distancia = abs(caravana->getLinha() - linha) +
+                        abs(caravana->getColuna() - coluna);
+
+            if (distancia <= 4) {
+                if (distancia < distanciaMin) {
+                    distanciaMin = distancia;
+                    alidado = caravana->getId();
+                    linhaAliado = caravana->getLinha();
+                    colunaAliado = caravana->getColuna();
+                }
+            }
+        }
+    }
+    // Se encontrar um caravana próxima, persegue-o
+    if (alidado) {
+        int novaLinha = linha + (rand() % 3 - 1);
+        int novaColuna = coluna + (rand() % 3 - 1);
+        distancia = abs(linhaAliado - novaLinha);
+                + abs(colunaAliado - novaColuna);
+
+        while (distancia>4) {
+         novaLinha = linha + (rand() % 3 - 1);
+         novaColuna = coluna + (rand() % 3 - 1);
+            distancia = abs(linhaAliado - novaLinha);
+                    + abs(colunaAliado - novaColuna);
+        }
+        std::cout<<ID<<" manteve-se perto da caravana "<<alidado<<"\n";
+
+        return std::make_pair(novaLinha, novaColuna);
+    }
+    int novaLinha = linha + (rand() % 3 - 1);
+    int novaColuna = coluna + (rand() % 3 - 1);
+    return std::make_pair(novaLinha, novaColuna);
+}
